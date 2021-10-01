b1795051093cbd7a668d2edb95fae599728cea5f
Alfonso Arriaga Quezada <luisaq95nin@gmail.com>
2021-10-01 09:36:37 -0500

    Implemented modularity of project files

--- server/index.js
+++ server/index.js
@@ -1,25 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
-
-
 app.use(cors());
-
 app.use(express.json()); // When we want to be able to accept JSON.
 
-app.get("/api/compliment", (req, res) => {
-  const compliments = ["Gee, you're a smart cookie!",
-					 "Cool shirt!",
-					 "Your Javascript skills are stellar.",
-  ];
+const serverController = require('./controllers/serverController');
 
-  // choose random compliment
-  let randomIndex = Math.floor(Math.random() * compliments.length);
-  let randomCompliment = compliments[randomIndex];
-
-  res.status(200).send(randomCompliment);
-  
-});
+app.get("/api/compliment", serverController.getCompliment);
 
 app.listen(4000, () => console.log("Server running on 4000"));
